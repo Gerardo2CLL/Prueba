5afa65cc43e22c153ba5a3fdda05384c90d95304
Gerardo Llamas <tu-correo@ejemplo.com>
2026-08-19 20:00:58 -0600

    componente tabla genral

---
+++ frontend/src/app/shared/tabla-general/tabla-general.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-general',
+  imports: [],
+  templateUrl: './tabla-general.component.html',
+  styleUrl: './tabla-general.component.css'
+})
+export class TablaGeneralComponent {
+
+}
