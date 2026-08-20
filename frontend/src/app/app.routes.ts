38473358ecd234fb175e822ac6bd3bed00e991b0
Gerardo Llamas <tu-correo@ejemplo.com>
2026-08-19 19:56:32 -0600

    agregar componente de inicio nuevo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: '',
+    loadComponent: () =>
+      import('./pages/inicio/inicio.component')
+        .then(m => m.InicioComponent)
+  }
+];
